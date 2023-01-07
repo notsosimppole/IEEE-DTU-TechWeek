d314c96f27e2c3d808d7f33d9c27705b2cff3900
SankalpGautam <92197386+SankalpGautam@users.noreply.github.com>
2023-01-07 13:44:11 +0530

    Update index.jsx

--- components/events/index.jsx
+++ components/events/index.jsx
@@ -15,8 +15,9 @@ const Events = () => {
                 <div className='h-2 w-10 bg-gradient-to-tl from-[#3376BD] to-[#08D4F8] rounded-full my-5'>
                 </div>
                 <p className='text-[#BECACA] pt-1 text-xl  px-10 md:px-0 py-10 md:py-0'>
-                    "TECHWEEK is the annual workshop galore organised by IEEE DTU which offers budding engineers an insight into the various new and upcoming technologies. Workshops organized during TECHWEEK introduce technologies to students and intent to stimulate their interest in cutting edge technologies of today. TECHWEEK is a true knowledge fair for all those who have in them a craving to learn, with workshops ranging from web-designing to robotics, from programming to Graphic designing."
-                    <br> "You don't need to be an IEEE DTU member to attend. Free for all!"
+                    TECHWEEK is the annual workshop galore organised by IEEE DTU which offers budding engineers an insight into the various new and upcoming technologies. Workshops organized during TECHWEEK introduce technologies to students and intent to stimulate their interest in cutting edge technologies of today.
+                    TECHWEEK is a true knowledge fair for all those who have in them a craving to learn, with workshops ranging from web-designing to robotics, from programming to Graphic designing.
+                    <br> You don't need to be an IEEE DTU member to attend. Free for all!
                 </p>
             </div>
         </div>
