657c993149a2d3a31a836365e74eea33a9e1ae5b
SankalpGautam <92197386+SankalpGautam@users.noreply.github.com>
2023-01-14 00:34:46 +0530

    Update index.jsx

--- components/sponsers/index.jsx
+++ components/sponsers/index.jsx
@@ -7,7 +7,7 @@ const Sponsers = () => {
         <div className='flex bg-[#414141] rounded-lg items-center justify-center w-full mx-4 gap-2 h-full flex-col md:flex-row'>
             <div className='flex flex-col items-center justify-center w-full mx-4 gap-2 h-full'>
                 <h2>
-                    <span className='text-4xl md:text-5xl font-bold bg-gradient-to-tl from-[#3376BD] to-[#08D4F8] inline-block text-transparent bg-clip-text font-prompt pt-6'>Our Sponsers</span>
+                    <span className='text-4xl md:text-5xl font-bold bg-gradient-to-tl from-[#3376BD] to-[#08D4F8] inline-block text-transparent bg-clip-text font-prompt pt-6'>Our Sponsors</span>
                 </h2>
                 <div className='h-2 w-10 bg-gradient-to-tl from-[#3376BD] to-[#08D4F8] rounded-full my-5'>
                 </div>
